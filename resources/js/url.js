74dfe28ee124885fe2db39c047d428fbcebe2e14
Miguel Angel Escajadillo Quispe <smcorporativo03@gmail.com>
2020-03-07 21:44:48 -0500

    corrigiendo url

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,5 +1,5 @@
-// const mainUrl='https://asf4ggd6.herokuapp.com/'
-const mainUrl='http://127.0.0.1:8000'
+const mainUrl='https://asf4ggd6.herokuapp.com/'
+// const mainUrl='http://127.0.0.1:8000'
 const apiUrl= mainUrl+'/api'
 
 const url={
