6396e1a92b6335f5bdfe16293cf538c92f41e9aa
Miguel Angel Escajadillo Quispe <smcorporativo03@gmail.com>
2020-03-07 22:07:38 -0500

    fix controller condiciones ventas

--- app/Http/Controllers/tb_condicionventas_controller.php
+++ app/Http/Controllers/tb_condicionventas_controller.php
@@ -10,7 +10,7 @@ class tb_condicionventas_controller extends Controller
   public function index(){
     $tb_condicionventa_obj = tb_condicionventa_obj::get();
     $result = new class{};
-    $result->clientes =  $tb_condicionventa_obj;
+    $result->condicionesventas =  $tb_condicionventa_obj;
     return response()->json($result,200);
   }
 }
